70566c947962256b3a9786e9b9c81b51fe804fc9
Derek Trider <derek.trider@securekey.com>
2019-12-02 17:26:03 -0500

    feat: CLI Cobra Support
    Created basic starter files, which provides a foundation for future commits to build off of. Allows one to start an edge-node instance via command line or environment variables. Right now it simply creates an underlying agent that runs on the user-defined host url with default parameters.
    
    Signed-off-by: Derek Trider <Derek.Trider@securekey.com>

---
+++ cmd/edge-store-rest/go.mod
@@ -0,0 +1,13 @@
+// Copyright SecureKey Technologies Inc. All Rights Reserved.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+module github.com/trustbloc/edge-store/cmd/edge-store-rest
+
+go 1.13
+
+require (
+	github.com/gorilla/mux v1.7.3
+	github.com/spf13/cobra v0.0.5
+	github.com/stretchr/testify v1.4.0
+)
--- scripts/check_unit.sh
+++ scripts/check_unit.sh
@@ -4,11 +4,31 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 set -e
 
-# Packages to exclude
+echo "Running $0"
+
+go generate ./...
+pwd=`pwd`
+echo "" > "$pwd"/coverage.txt
+
+amend_coverage_file () {
+if [ -f profile.out ]; then
+     cat profile.out >> "$pwd"/coverage.txt
+     rm profile.out
+fi
+}
+
+# Running edge-store unit tests
 PKGS=`go list github.com/trustbloc/edge-store/... 2> /dev/null | \
                                                   grep -v /mocks`
-echo "Running pkg unit tests..."
-go test -count=1 -cover $PKGS -p 1 -timeout=10m -race -coverprofile=coverage.txt -covermode=atomic
+go test $PKGS -count=1 -race -coverprofile=profile.out -covermode=atomic -timeout=10m
+amend_coverage_file
+
+# Running edge-store-rest unit tests
+cd cmd/edge-store-rest
+PKGS=`go list github.com/trustbloc/edge-store/cmd/edge-store-rest/... 2> /dev/null | \
+                                                 grep -v /mocks`
+go test $PKGS -count=1 -race -coverprofile=profile.out -covermode=atomic -timeout=10m
+amend_coverage_file
+cd "$pwd" || exit
